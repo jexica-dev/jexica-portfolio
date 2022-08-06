6b2e60900ff4f5ee9dc212b8dd5bc2eab14899d5
Jexica Ayran <jexica@jexica-mac.myfiosgateway.com>
2022-08-06 17:28:23 -0400

    video on modals and copywriting.

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -6,7 +6,6 @@ export default function Modal(props) {
   return (
     <div className="p-5 overflow-y-scroll">
       <p className=" text-left font-whyteinktrap lg:text-3xl pb-2">
-        {" "}
         {props.modalData.title}
       </p>
 
@@ -15,14 +14,9 @@ export default function Modal(props) {
       </p>
 
       <div className="">
-        <Image
-          className={"absolute z-10"}
-          src={props.modalData.img}
-          width={500}
-          height={250}
-          alt={"image"}
-          layout="responsive"
-        />
+        <video className="" width="100%" height="auto" autoPlay loop>
+          <source src={props.modalData.video} type="video/mp4" />
+        </video>
       </div>
 
       <div className="pt-2 text-xs underline underline-offset-4 font-whytemono text-left flex flex-row justify-around w-1/2">
--- pages/work/index.js
+++ pages/work/index.js
@@ -64,11 +64,9 @@ function ProjectListPage(props) {
             className={
               !modalOpen
                 ? "hidden w-screen h-screen fixed"
-                : "w-screen h-screen fixed  mix-blend-exclusion bg-black opacity-80 filter blur-3xl"
+                : "w-screen h-screen fixed bg-black opacity-80 filter blur-3xl"
             }
-          >
-            test
-          </div>
+          ></div>
           <div
             style={{ top: "10vh", display: "flex" }}
             className={
@@ -78,7 +76,7 @@ function ProjectListPage(props) {
             }
           >
             <div className="fixed  w-140  h-fit bg-white ">
-              <Modal modalData={modalData} />
+              {!modalOpen ? null : <Modal modalData={modalData} />}
             </div>
           </div>
         </div>
@@ -94,7 +92,7 @@ function ProjectListPage(props) {
             title="digi-Altar"
             date="10-2021"
             info="full-stack web app for creating cyber art-shrine collages, using RoR and React.js"
-            body="digi-Altar is a cyber spiritual space, where you can create, share, and inspire your own altar using a hybrid mix of relics and artifacts. Images may link to actual objects that you may purchase and add to your own physical sanctuary."
+            body="digi-Altar is a cyber spiritual space, where you can create, share, and inspire your own altar using a hybrid mix of relics and artifacts."
             img="/digialtar.png"
             video="/digialtar.mp4"
             alt="digialtar"
@@ -111,10 +109,7 @@ function ProjectListPage(props) {
             info="e-commerce (MERN stack) website for creating, modifying, and purchasing customizable organic jams"
             lang="HTML, CSS, JavaScript, React, Bootstrap, Node. Express, MongoDB,
             Mongoose"
-            body="g’JAM is an eCommerce shop that sells customizable sweet and
-            spicy, freshly-made jams, using a unique variety of ingredients.
-            Users, after creating an account and signing in, are able to buy
-            (shopping cart), create, and edit jams."
+            body="g’JAM is an eCommerce shop that sells customizable freshly-made jams, with music themed names and an endless list of savory, spicy, and sweet ingredients. Users, after creating an account and signing in, are able to buy (shopping cart), create, and edit jams."
             img="/gjam.png"
             video="/gjam.mp4"
             alt="gjam"
@@ -127,7 +122,7 @@ function ProjectListPage(props) {
             setModalOpen={setModalOpen}
             setModalData={setModalData}
             title="Thyme"
-            date="09-2021"
+            date="08-2021"
             info={
               <>
                 a React web app for creating a bucket/task list based on your
@@ -138,7 +133,7 @@ function ProjectListPage(props) {
             }
             lang="HTML, CSS, JavaScript, React, Airtable"
             body="Thyme is a modern and simple tasklist, with a leaf-branching
-            system, for goal-setting or bucket lists."
+            system, for goal-setting or bucket lists. Airtable (backend) is collecting the tasks that submitted from the form, while React (frontend) is rendering the data and the single-page app. "
             img="/thyme.png"
             video="/thyme.mp4"
             alt="thyme"
@@ -153,22 +148,22 @@ function ProjectListPage(props) {
             title="#36daysoftype_22"
             lang="HTML, CSS, Javascript, React, Touchdesigner "
             info="a React web app designed for a personal project (audio-reactive alphabet typography), participating in the online event: #36daysoftype"
-            body="a React web app designed for a personal project (audio-reactive alphabet typography), participating in the online event: #36daysoftype"
+            body="a React web app designed for a personal project (audio-reactive alphabet typography), participating in the online event: #36daysoftype. Touchdesigner, a node and code based software, specializes in creative programming and is reflective of the work I would love to be a part of."
             img="/36days.png"
             video="/36days.mp4"
             alt="36days"
-            site="36daysoftype.jexica.design/"
+            site="https://36daysoftype.jexica.design/"
             github="https://github.com/xicaz/36days_2022"
           />
 
           <ProjectCard
             setModalOpen={setModalOpen}
             setModalData={setModalData}
-            date="07-2022"
+            date="03-2022"
             title="Ghibli-Giphy Remix"
             lang="HTML, CSS, Javascript"
             info="Ghibli film informational (wiki) page abstractly layered with images and text collected and rendered through the Ghibli Studio and Giphy API"
-            body="Ghibli film informational (wiki) page abstractly layered with images and text collected and rendered through the Ghibli Studio and Giphy API"
+            body="A drop-down menu holding a list of Ghibli films offers an informational (wiki) page abstractly layered with gif images and text. Data is collected and rendered through the Ghibli Studio and Giphy API."
             img="/giphy.png"
             video="/giphy.mp4"
             alt="ghibli-giphy"
@@ -179,10 +174,10 @@ function ProjectListPage(props) {
           <ProjectCard
             setModalOpen={setModalOpen}
             setModalData={setModalData}
-            date="06-2021"
+            date="07-2021"
             title="Spirit Gif"
             info="giphy images of the user's name are expressively collaged, using the Giphy API"
-            body="giphy images of the user's name are expressively collaged, using the Giphy API"
+            body="After entering your name in the search bar, giphy images are expressively and randomly added and collaged using the Giphy API."
             img="/spiritgif.png"
             video="/spiritgif.mp4"
             alt="spiritgif"
