cedf4fce4c543975ce59be2c17729baf2afddf5b
Jexica Ayran <jexica@jexica-mac.myfiosgateway.com>
2022-12-17 03:35:18 -0500

    drag functionality added to homepg and updated grid media queries with skill buttons.

--- components/Buttons/GlassButton.js
+++ components/Buttons/GlassButton.js
@@ -2,6 +2,8 @@ import Link from "next/dist/client/link";
 import React, { useState } from "react";
 import Marquee from "react-fast-marquee";
 import Image from "next/image";
+import Draggable from "react-draggable";
+import SkillButtons from "./SkillButton";
 
 export default function GlassButton() {
   const [onHover, setOnHover] = useState(false);
@@ -9,7 +11,7 @@ export default function GlassButton() {
   const [onHover2, setOnHover2] = useState(false);
   const arrow = "→  ";
   return (
-    <div className=" ">
+    <div className=" cursor-star">
       {/*  */}
       {/* <div
         style={{
@@ -35,67 +37,75 @@ export default function GlassButton() {
       </div> */}
 
       {/*  */}
-      <div className=" grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-3  place-items-center w-full h-full px-24 pt-20 pb-14">
+      <div className=" grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3  place-items-center w-full h-full px-20 py-16 ">
         {/*  */}
 
         <div className="grid grid-cols-1 gap-3  h-full place-self-end ">
           {/*  */}
-          <div className="z-10 text-2xl  w-96  ">
-            <div
-              onMouseEnter={() => setOnHover1(true)}
-              onMouseLeave={() => setOnHover1(false)}
-              // style={
-              //   onHover1 ? { filter: "drop-shadow( .45vw .45vw 0 black)" } : null
-              // }
-              className=" w-full animate-fadein-02 text-right py-10 px-30   mix-blend-overlay flex justify-around rounded-3xl bg-cover bg-left-top bg-gradient01 "
-            >
-              <p className=" text-black pr-5  mix-blend-overlay  tracking-wide text-8xl leading-15  font-magtis  ">
-                jexica ayran
-              </p>
-            </div>
-          </div>
-          {/*  */}
-          <div
-            onMouseEnter={() => setOnHover(true)}
-            onMouseLeave={() => setOnHover(false)}
-            // style={
-            //   onHover ? { filter: "drop-shadow( .45vw .45vw 0 black)" } : null
-            // }
-            className=" w-96 rounded-3xl bg-cover bg-right-top bg-gradient04 hover:text-indigo-700 px-10 pb-3 pt-10 text-3xl tracking-wide text-left  text-black font-whytemono  "
-          >
-            <p className=" text-6xl font-whyteinktrap ">
-              <span className="  mix-blend-overlay filter ">
-                Creative Developer
-              </span>
-              <div className="flex flex-row  pt-3 pr-10  pb-5  justify-end">
-                <div className=" text-3xl pr-5 pt-3 animate-bounce-slow text-yellow-300 ">
-                  {arrow}
-                </div>
-                <p className=" mt-1 z-40 flex justify-around  text-white rounded-full py-1 px-7 whitespace-nowrap font-bold tracking-wide bg-trp border  text-2xl font-whytemono  border-black">
-                  <div
-                    className=" w-44"
-                    onMouseEnter={() => setOnHover(true)}
-                    onMouseLeave={() => setOnHover(false)}
-                  >
-                    <Link href="/work" passHref>
-                      <a className=" hover:no-underline hover:text-yellow-300">
-                        <Marquee
-                          style={{ textAlign: "center" }}
-                          direction="right"
-                          className="overflow-hidden p-1 "
-                          gradient={null}
-                          speed={30}
-                          pauseOnHover
-                        >
-                          projects
-                        </Marquee>
-                      </a>
-                    </Link>
-                  </div>
+          <Draggable>
+            <div className=" backdrop-blur-3xl z-10 text-2xl place-self-end ">
+              <div
+                onMouseEnter={() => setOnHover1(true)}
+                onMouseLeave={() => setOnHover1(false)}
+                // style={
+                //   onHover1 ? { filter: "drop-shadow( .45vw .45vw 0 black)" } : null
+                // }
+                className=" w-full animate-fadein-02 text-right py-10 px-30  bg-gradient01  mix-blend-overlay flex rounded-3xl bg-cover bg-left-top  "
+              >
+                <p
+                  style={{ fontSize: "5.25rem" }}
+                  className=" text-indigo-900 px-5 pl-3 mix-blend-overlay  tracking-wide  leading-15  font-whyteinktrap font-bold  "
+                >
+                  jexica ayran
                 </p>
               </div>
-            </p>
-          </div>
+            </div>
+          </Draggable>
+          {/*  */}
+
+          <Draggable>
+            <div
+              onMouseEnter={() => setOnHover(true)}
+              onMouseLeave={() => setOnHover(false)}
+              // style={
+              //   onHover ? { filter: "drop-shadow( .45vw .45vw 0 black)" } : null
+              // }
+              className="place-items-start animate-fadein-02  rounded-3xl bg-cover bg-right-top bg-gradient04 hover:text-indigo-700 px-10 pb-3 pt-10 text-3xl tracking-wide text-left  text-black   "
+            >
+              <p className="">
+                <span className=" text-5xl mix-blend-overlay filter leading-12  font-whytemono">
+                  Creative Developer
+                </span>
+                <div className="flex flex-row flex-wrap pt-2 justify-end">
+                  <div className=" text-3xl pr-2 pt-3 animate-bounce-slow text-yellow-300 ">
+                    {arrow}
+                  </div>
+                  <p className=" mt-1 z-40 flex justify-around  text-white rounded-full py-1 px-5 whitespace-nowrap font-bold tracking-wide bg-trp border  text-2xl font-whytemono  border-black">
+                    <div
+                      className=" w-44"
+                      onMouseEnter={() => setOnHover(true)}
+                      onMouseLeave={() => setOnHover(false)}
+                    >
+                      <Link href="/work" passHref>
+                        <a className=" hover:no-underline hover:text-yellow-300">
+                          <Marquee
+                            style={{ textAlign: "center" }}
+                            direction="right"
+                            className="overflow-hidden p-1 "
+                            gradient={null}
+                            speed={30}
+                            pauseOnHover
+                          >
+                            projects
+                          </Marquee>
+                        </a>
+                      </Link>
+                    </div>
+                  </p>
+                </div>
+              </p>
+            </div>
+          </Draggable>
           {/*  */}
 
           {/*  */}
@@ -103,41 +113,44 @@ export default function GlassButton() {
         </div>
         {/*  */}
 
-        <div
-          onMouseEnter={() => setOnHover2(true)}
-          onMouseLeave={() => setOnHover2(false)}
-          // style={
-          //   onHover2 ? { filter: "drop-shadow( .45vw .45vw 0 black)" } : null
-          // }
-          className="place-self-start w-96 animate-fadein-02 flex  p-10   h-fit rounded-3xl bg-cover bg-right-top bg-gradient06 "
-        >
-          <div className="text-xl tracking-wide text-left  text-black font-whytemono selection:bg-fuchsia-300 selection:text-fuchsia-900 ">
-            Hello, <br className="mb-3" />
-            I’m{" "}
-            <span className=" font-whyteinktrap tracking-wider">
-              Jexica Ayran
-            </span>
-            , <br />a{" "}
-            <span className=" font-monument text-red-400 ">designer</span> and{" "}
-            <span className=" font-monument  text-red-400 ">
-              web developer{" "}
-            </span>
-            originally from the SF Bay Area and now currently residing in NYC.
-            <p className="mt-3 text-base">
-              <span className="text-yellow-300">&#10022;</span> California
-              College of the Arts (SF, CA):
-              <br /> Bachelor of Architecture, 2016.
-            </p>
-            <p className="mt-3 text-base">
-              <span className="text-yellow-300">&#10022;</span> General
-              Assembly: Software Engineering Immersive, 2021.
-              <span className="mt-3 block">
-                * Available for freelance and remote work.
+        <Draggable>
+          <div
+            onMouseEnter={() => setOnHover2(true)}
+            onMouseLeave={() => setOnHover2(false)}
+            // style={
+            //   onHover2 ? { filter: "drop-shadow( .45vw .45vw 0 black)" } : null
+            // }
+            className="place-self-end  animate-fadein-02 flex p-10  h-fit rounded-3xl bg-cover bg-right-top bg-gradient06 "
+          >
+            <div className="text-xl tracking-wide text-left  text-black font-whytemono selection:bg-fuchsia-300 selection:text-fuchsia-900 ">
+              Hello, <br className="mb-3" />
+              I’m{" "}
+              {/* <span className=" font-whyteinktrap  text-red-400 tracking-wider">
+                Jexica Ayran
+              </span> */}
+              a <span className=" font-monument text-red-400 ">designer</span>{" "}
+              and{" "}
+              <span className=" font-monument  text-red-400 ">
+                web developer{" "}
               </span>
-            </p>
+              originally from the SF Bay Area and now currently residing in NYC.
+              <p className="mt-3 text-base">
+                <span className="text-yellow-300">&#10022;</span> California
+                College of the Arts (SF, CA):
+                <br /> Bachelor of Architecture, 2016.
+              </p>
+              <p className="mt-3 text-base">
+                <span className="text-yellow-300">&#10022;</span> General
+                Assembly: Software Engineering Immersive, 2021.
+                <span className="mt-3 block">
+                  * Available for freelance and remote work.
+                </span>
+              </p>
+            </div>
           </div>
-        </div>
+        </Draggable>
         {/*  */}
+        <SkillButtons />
 
         {/*  */}
       </div>
