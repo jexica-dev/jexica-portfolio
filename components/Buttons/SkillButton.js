9b8930496be0ec74c6b21ea093a1d21ebf3e201d
Jexica Ayran <jexica@jexica-mac.myfiosgateway.com>
2022-12-17 03:32:40 -0500

    organized grid structure and updated skill buttons content. randomized styling and typefaces

--- components/Buttons/SkillButton.js
+++ components/Buttons/SkillButton.js
@@ -1,38 +1,79 @@
 import React from "react";
 // import "../Buttons/SkillButtons.css";
+import Draggable from "react-draggable";
 
 export default function SkillButtons(props) {
   const defaultSkillBtn =
-    " flex justify-around text-white rounded-full py-3 px-10 mx-3 whitespace-nowrap font-medium tracking-widest bg-black text-2xl mb-5";
+    "  bg-fit bg-cover mix-blend-hard-light flex justify-around text-black  rounded-full py-3 px-10  whitespace-nowrap font-medium tracking-widest text-xl ";
+
+  let languages = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Node.js",
+    "ReactJS",
+    "MongoDB",
+    "ExpressJS",
+    "TailwindCSS",
+    "Figma",
+    "Bootstrap",
+    "MaterialUI",
+    "Ruby",
+    "RubyonRails",
+    "NextJS",
+    "Touchdesigner",
+    "Adobe CC",
+  ];
+
+  let skillButtons = [];
+
+  // WILL FIX THIS NEXT TIME
+
+  let gradientIndex = [
+    "bg-gradient01",
+    "bg-gradient02",
+    "bg-gradient03",
+    "bg-gradient04",
+    "bg-gradient05",
+    "bg-gradient06",
+    "bg-gradient07",
+    "bg-gradient08",
+    "bg-gradient09",
+  ];
+
+  let fonts = [
+    "font-whyteinktrap",
+    "font-ginto",
+    "font-gintonord",
+    "font-mono",
+    "font-prophet",
+    "font-social",
+    "font-whytemono",
+    "font-monument",
+    "font-whyteinktrap",
+  ];
+
+  for (let i = 0; i < languages.length; i++) {
+    skillButtons.push(
+      <Draggable>
+        <div
+          className={
+            defaultSkillBtn +
+            fonts[i % fonts.length] +
+            " " +
+            gradientIndex[i % 8]
+          }
+        >
+          {languages[i]}
+        </div>
+      </Draggable>
+    );
+  }
 
   return (
     <>
-      <div className=" animate-fadein w-screen h-full flex flex-wrap flex-row justify-center">
-        <div className="w-container justify-center flex content-start flex-wrap flex-row  items-start ">
-          <div className={"w-28 font-ginto" + defaultSkillBtn}>HTML</div>
-          <div className={" w-28" + defaultSkillBtn}>CSS</div>
-          <div className={"w-48" + defaultSkillBtn}>Javascript</div>
-          <div className={"w-30" + defaultSkillBtn}>Node.js</div>
-        </div>
-        <div className="w-container justify-center flex content-start flex-wrap flex-row  items-start  ">
-          <div className={"w-44" + defaultSkillBtn}>React.js</div>
-          <div className={" w-48" + defaultSkillBtn}>MongoDB</div>
-          <div className={"w-44" + defaultSkillBtn}>Express.js</div>
-          <div className={"w-48" + defaultSkillBtn}>Tailwind CSS</div>
-        </div>
-        <div className="w-container justify-center flex content-start flex-wrap flex-row  items-start ">
-          <div className={"w-48" + defaultSkillBtn}>Bootstrap</div>
-          <div className={"w-28" + defaultSkillBtn}>Java</div>
-
-          <div className={"w-28" + defaultSkillBtn}>Ruby</div>
-          <div className={"w-52" + defaultSkillBtn}>Ruby on Rails</div>
-        </div>
-        <div className="w-container justify-center flex content-start flex-wrap flex-row  items-start ">
-          <div className={" w-32" + defaultSkillBtn}>Next.js</div>
-          <div className={" w-64" + defaultSkillBtn}>React Three Fiber</div>
-          <div className={"w-48" + defaultSkillBtn}>Spring Boot</div>
-          <div className={"w-52" + defaultSkillBtn}>Material UI</div>
-        </div>
+      <div className="pl-3 place-items-stretch animate-fadein lg:w-96 md:w-full sm:w-full lg:col-span-1 md:col-span-2">
+        <div className="grid grid-cols-2 gap-3">{skillButtons}</div>
       </div>
     </>
   );
