e7731cbf20064f1a443d16de12e2899b34c04263
jexica ayran <jess.ayran@gmail.com>
2026-02-07 10:37:24 -0800

    added theweatherapp and fixed css

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,16 +1,14 @@
-import React, { createRef } from "react";
-import Navbar from "../navbar/Navbar";
-import styles from "../../styles/Layout.module.css";
+import React, { createRef } from 'react';
+import Navbar from '../navbar/Navbar';
+import styles from '../../styles/Layout.module.css';
 // import AnimatedCursor from "react-animated-cursor";
 
 export default function Layout(props) {
-
-
   return (
     <>
       <Navbar />
 
-      <div className={[styles.container + " absolute "]}>
+      <div className={[styles.container]}>
         <div className="relative">{props.children}</div>
       </div>
     </>
